e2610355fbae9e1268df5dddefeb61a3d7876181
nta1998 <111499645+nta1998@users.noreply.github.com>
2024-08-01 13:01:59 +0300

    fix 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export const metadata: Metadata = {
     description: DATA.description,
     url: DATA.url,
     siteName: `${DATA.name}`,
+    images:DATA.img,
     locale: "en_US",
     type: "website",
   },
